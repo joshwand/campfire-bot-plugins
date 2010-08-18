c062ba94589204faba971083eb6bc1e9fda16756
joshwand <josh@joshwand.com>
2010-08-18 13:19:29 -0400

    update chuck norris plugin to use new website format

--- chuck.rb
+++ chuck.rb
@@ -4,33 +4,20 @@ require 'hpricot'
 class Chuck < CampfireBot::Plugin
   on_command 'chuck', :chuck
   
+  def initialize
+    @log = Logging.logger["CampfireBot::Plugin::Chuck"]
+  end
+  
   def chuck(msg)
-    # Get our sayings from a Chuck Norris facts page (one of 9 to-date)
-    url = "http://www.chucknorrisfacts.com/page#{rand(8)+1}.html"
-    response = ''
-    fact_file = Array.new
-
-    begin
-      # open-uri RDoc: http://stdlib.rubyonrails.org/libdoc/open-uri/rdoc/index.html
-      open(url, "User-Agent" => "Ruby/#{RUBY_VERSION}",
-        "From" => "Campfire") { |f|
-                                  
-        # Save the response body
-        response = f.read
-      }
+    url = "http://www.chucknorrisfacts.com/all-chuck-norris-facts?page=#{rand(172)+1}"
+    doc = Hpricot(open(url))
     
-      # HPricot RDoc: http://code.whytheluckystiff.net/hpricot/
-      doc = Hpricot(response)
+    facts = []
     
-      # Pull out all the facts and load them into an array
-      (doc/"*/li").each do |fact|
-         fact_file << "#{fact.inner_html}"
-      end
-
-      # Select a random entry in the array, clean the string and output it  
-      msg.speak(fact_file[rand(fact_file.size)].gsub(/<\/?[^>]*>/,"").gsub(/\s+/," ").gsub(/\&quote;/,"'").gsub(/\&[\#|\w]\w+\;/,""))
-    rescue Exception => e
-      msg.speak(e, "\n")
+    (doc/".item-list a.createYourOwn").each do |a_tag|
+      facts << CGI.unescapeHTML(a_tag.inner_html)
     end
+    
+    msg.speak(facts[rand(facts.size)])
   end
 end
