cd9227e1a07124c38028da22353d3714e7c4d050
Michael MacDonald <michaelm@amc.org.au>
2010-11-05 10:27:37 +1100

    Updated weather plugin

--- weather.rb
+++ weather.rb
@@ -4,18 +4,21 @@ class Weather < CampfireBot::Plugin
   on_command 'weather', :weather
   
   def weather(msg)
-    city = {
-      'adelaide'  => 'ASXX0001',
-      'brisbane'  => 'ASXX0016',
-      'canberra'  => 'ASXX0023',
-      'darwin'    => 'ASXX0032',
-      'hobart'    => 'ASXX0057',
-      'melbourne' => 'ASXX0075',
-      'perth'     => 'ASXX0231',
-      'sydney'    => 'ASXX0112'
-    }[(msg[:message].split(' ')[1] || 'canberra').downcase]
+    cities = {
+      'adelaide'  => '1099805',
+      'brisbane'  => '1100661',
+      'canberra'  => '1100968',
+      'darwin'    => '1101597',
+      'hobart'    => '1102670',
+      'melbourne' => '1103816',
+      'perth'     => '1098081',
+      'sydney'    => '1105779'
+    }
     
-    data = YahooWeather::Client.new.lookup_location(city, 'c')
+    city_id = cities[(msg[:message]).downcase]  # select city, or
+    city_id ||= cities['canberra']              # use default if no matches
+    
+    data = YahooWeather::Client.new.lookup_by_woeid(city_id, 'c')
     
     msg.speak("#{data.title} - #{data.condition.text}, #{data.condition.temp} deg C (high #{data.forecasts.first.high}, low #{data.forecasts.first.low})")
   end
